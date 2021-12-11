6174eb679e2fa8a239f33d13a1786d85546afd2c
dietervansteenwegen <github@vansteenwegen.org>
2021-12-11 15:20:27 +0100

    PEP8 and file rename

--- quickmail/GetConfig.py
+++ quickmail/GetConfig.py
@@ -2,16 +2,20 @@ from configparser import ConfigParser
 from typing import Dict, Tuple
 
 
-def getCredentials():
+def get_config():
     parser = ConfigParser()
     parser.read('./quickmail/credentials')
     rtnDict = {}
     for sect in parser.sections():
         for key, value in parser.items(sect):
-            rtnDict[key] = value
+            rtnDict[key] = value.strip("'")
 
     return rtnDict
 
 
-def checkMissing(credentials: Dict, required: Tuple):
-    return [req for req in required if len(credentials[req]) < 1]
+def check_missing(cfg: Dict, required: Tuple):
+    return [req for req in required if len(cfg.get(req, '')) < 1]
+
+
+def create_blank():
+    pass
--- quickmail/quickmail.py
+++ quickmail/quickmail.py
@@ -1,39 +1,39 @@
-from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5 import QtCore, QtGui, QtWidgets
 from MainWindow import Ui_QuickMail
-import GetCredentials
+import GetConfig
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_QuickMail):
     def __init__(self, *args, obj=None, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
-        self._getConfig()
-        self.setupWindow()
-        self.btn_parse_cred.released.connect(self._getConfig)
-        self.btn_send.released.connect(self._sendEmail)
+        self._get_config()
+        self.btn_parse_cred.released.connect(self._get_config)
+        self.btn_send.released.connect(self._send_email)
 
-    def _getConfig(self):
-        self.credentials = GetCredentials.getCredentials()
-        missing = GetCredentials.checkMissing(self.credentials,
-                                              ('server', 'pw', 'login', 'recipient'))
+    def _get_config(self):
+        self.credentials = GetConfig.get_config()
+        missing = GetConfig.check_missing(self.credentials, ('server', 'pw', 'login', 'recipient'))
         if len(missing) > 0:
             self._credentials_ok = False
             self.btn_send.setEnabled(False)
             for i in [self.lbl_server, self.lbl_user, self.lbl_recipient]:
                 i.setText('credentials file not complete')
+            self.statusbar.showMessage(f'Credentials missing: {missing}!')
             msgbox = QtWidgets.QMessageBox.critical(
                 self,
                 'Missing credentials',
                 f'Missing credentials: {missing}\nPlease edit the credentials file.',
                 buttons=QtWidgets.QMessageBox.Ok)
         else:
+            self.statusbar.showMessage('')
             self._credentials_ok = True
             self.btn_send.setEnabled(True)
             self.lbl_server.setText(self.credentials['server'])
             self.lbl_user.setText(self.credentials['login'])
             self.lbl_recipient.setText(self.credentials['recipient'])
 
-    def _sendEmail(self):
+    def _send_email(self):
         txt = self.txt_mail.toPlainText()
         if len(txt) < 1:
             msgbox = QtWidgets.QMessageBox.warning(
@@ -42,8 +42,11 @@ class MainWindow(QtWidgets.QMainWindow, Ui_QuickMail):
                 'The message box is empty, no message to be sent...',
                 buttons=QtWidgets.QMessageBox.Ok)
         else:
-            pass
-            # self.send()
+            self._handleEmail(txt)
+            self.statusbar.showMessage(f'Send email: {txt}')
+
+    def _handleEmail(self, txt):
+        print(f'message sent: {txt}')
 
 
 app = QtWidgets.QApplication([])
